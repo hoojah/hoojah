968c8a94b2dd6f179180c184e488c5921ec7ef45
Rudzainy <rudzainy@gmail.com>
2020-10-02 00:31:37 +0800

    Create notification when child hoojah & new votes are created

--- app/javascript/components/Notifications/card.js
+++ app/javascript/components/Notifications/card.js
@@ -2,6 +2,7 @@ import React from "react"
 import { Link } from 'react-router-dom'
 import AnnouncementIcon from '../Icons/announcement'
 import HujahIcon from '../Icons/hujah'
+import VoteIcon from '../Icons/votes'
 
 class NotificationCard extends React.Component {
 
@@ -31,7 +32,7 @@ class NotificationCard extends React.Component {
       )
     } else if(category == "new_hoojah_response"){
       return(
-        <Link to={`/hoojah/${hujah.id}`}className="no-underscore">
+        <Link to={`/hoojah/${hujah.id}`} className="no-underscore">
           <div className={`shadow card-body border-left-8 border-unread border-bottom-0 py-0 pl-0 pr-2 d-flex align-items-center mb-1`}>
             <div className="media py-1">
               <div className="media-body">
@@ -39,6 +40,26 @@ class NotificationCard extends React.Component {
                   <div className="d-flex align-items-center text-14 card-body btn-icon-14 text-grey fill-grey py-0">
                     <HujahIcon /> 
                     <span className="ml-1">@{subject_user.username} posted a new argument on your hoojah </span>
+                  </div>
+                  <div className="text-14 card-body py-0 text-truncate d-block d-sm-none" style={{ maxWidth: '270px' }} dangerouslySetInnerHTML={{ __html: hujah.body }}></div>
+                  <div className="text-14 card-body py-0 text-truncate d-none d-sm-block d-md-none" style={{ maxWidth: '310px' }} dangerouslySetInnerHTML={{ __html: hujah.body }}></div>
+                  <div className="text-14 card-body py-0 text-truncate d-none d-md-block" style={{ maxWidth: '350px' }} dangerouslySetInnerHTML={{ __html: hujah.body }}></div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </Link>
+      )
+    } else if(category == "new_vote"){
+      return(
+        <Link to={`/hoojah/${hujah.id}`} className="no-underscore">
+          <div className={`shadow card-body border-left-8 border-unread border-bottom-0 py-0 pl-0 pr-2 d-flex align-items-center mb-1`}>
+            <div className="media py-1">
+              <div className="media-body">
+                <div className="d-flex flex-column">
+                  <div className="d-flex align-items-center text-14 card-body btn-icon-14 text-grey fill-grey py-0">
+                    <VoteIcon /> 
+                    <span className="ml-1">You have a new vote on your hoojah </span>
                   </div>
                   <div className="text-14 card-body py-0 text-truncate d-block d-sm-none" style={{ maxWidth: '270px' }} dangerouslySetInnerHTML={{ __html: hujah.body }}></div>
                   <div className="text-14 card-body py-0 text-truncate d-none d-sm-block d-md-none" style={{ maxWidth: '310px' }} dangerouslySetInnerHTML={{ __html: hujah.body }}></div>
