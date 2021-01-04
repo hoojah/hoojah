b669efc5446b2a87834d2951f8835e259497b2ce
Rudzainy <rudzainy@gmail.com>
2021-01-04 22:44:12 +0800

    Style pinned section

--- app/javascript/components/Hujah/pinned.js
+++ app/javascript/components/Hujah/pinned.js
@@ -7,13 +7,9 @@ class Pinned extends React.Component {
 
     return(
       <div className="shadow card border-0 rounded-0 mb-2">
-        <div className="card-header border-bottom-0 pb-0 d-flex justify-content-between align-items-center">
-          Create a Hoojah account to secure your @username now!
-        </div>
-        <div className="card-body pb-0">
-          <Link to={'/start/signup'}>
-            <h5 className="card-title">Click here to SIGN UP!</h5>
-          </Link>
+        <div className="card-body text-center bg-info text-white">
+          <h5 className="card-title">Create a Hoojah account<br />to secure your @username now!</h5>
+          <Link to={"/start/signup"} className="btn btn-warning">Click here to sign up!</Link>
         </div>
       </div>
     )
